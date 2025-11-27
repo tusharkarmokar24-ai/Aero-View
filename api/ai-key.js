5d8439024286f8a4ff3cada379366638c5756d47
tusharkarmokar24-ai <pank36720@gmail.com>
2025-11-27 13:47:45 +0530

    update

--- api/ai-key.js
+++ api/ai-key.js
@@ -3,9 +3,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
-export const config = {
-  runtime: "edge" // Vercel Edge Function
-};
 
 export default async function generateResponse(req) {
   try {
